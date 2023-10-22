e236bb7ed3a7cefafaf222a57dcc00cdb014a60c
Fatemeh madihi <54662177+fmadihi@users.noreply.github.com>
2023-10-22 18:14:41 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
-alert("hello");
+var d = new Date();
+var h = d.getHours();
+var m = d.getMinutes();
+var s = d.getSeconds();
+var t;
+setInterval(digit, 1000);
+function digit() {
+    if (s == 60) {
+        m++;
+        s = 0;
+        if (m == 60) {
+            h++;
+            m = 0;
+            if (h == 24) {
+                h = 0;
+            }
+        }
+    }
+    s++;
+    document.getElementById("hour").innerText = h;
+    document.getElementById("minute").innerText = m;
+    document.getElementById("second").innerText = s;
+}
+if (h > 12 && h < 24) {
+    h = h - 12;
+    t = "PM";
+}
+else if (h == 24) {
+    h = 0;
+    t = "AM";
+}
+else if (h == 12) {
+    h = 12;
+    t = "PM";
+}
+else {
+    t = "AM";
+}
+
+if (h < 10) {
+    h = "0" + h
+}
+document.getElementById("hour").innerText = h;
+document.getElementById("minute").innerText = m;
+document.getElementById("second").innerText = s;
+document.getElementById("AP").innerText = t;
